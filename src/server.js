ddbb55e0c966801c3dfd71e12187f6107a4ab7c0
Luiz Fernando <lfaires@gmail.com>
2021-06-17 11:50:16 -0300

    games routes

--- src/server.js
+++ src/server.js
@@ -43,12 +43,56 @@ app.post('/categories', async (req,res) => {
 
     try {
         const result = await connection.query('SELECT * FROM categories WHERE name = $1', [name])
-        console.log(result.rows.length)
         if(result.rows.length !== 0){
             res.sendStatus(409)
             return
         }
         await connection.query('INSERT INTO categories (name) VALUES ($1)',[name])
+        res.sendStatus(201)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+//Games route
+
+app.get('/games', async (req,res) => {
+    const { name } = req.query
+    const queryString = name ? ` WHERE name ILIKE '${name}%'` : ""
+    
+    try {
+        const result = await connection.query('SELECT * FROM games'+ queryString)
+        res.send(result.rows)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+app.post('/games', async (req,res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(30),
+        stockTotal: Joi.number().greater(0),  
+        pricePerDay: Joi.number().greater(0),
+    });
+
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    const { error } = schema.validate({name: name, stockTotal: stockTotal, pricePerDay: pricePerDay}); 
+    console.log(error) 
+    if (error !== undefined){
+        res.sendStatus(400)
+        return
+    }
+
+    try{
+        const result = await connection.query('SELECT * FROM categories WHERE id = $1', [categoryId])
+        console.log(result)
+        if(result.rows.length === 0){
+            res.sendStatus(409)
+            return
+        }
+        await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',[name, image, stockTotal, categoryId, pricePerDay])
         res.sendStatus(201)
     } catch (error) {
         console.log(error)
